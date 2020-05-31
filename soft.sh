39791129a34c586fa5dc4a7d8433b5a39cfc150f
QingHui653 <910944453@qq.com>
2020-05-31 15:57:53 +0800

    vssr 修改为 vssrplus

--- soft.sh
+++ soft.sh
@@ -20,13 +20,15 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus
 #helloworld
 # https://github.com/fw876/helloworld
 git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git  
+# git clone https://github.com/jerrykuku/luci-app-vssr.git  
 # rm luci-app-vssr/root/etc/china_ssr.txt
 # rm luci-app-vssr/root/etc/config/black.txt
 # rm luci-app-vssr/root/etc/config/white.txt
 # rm luci-app-vssr/root/etc/dnsmasq.oversea/oversea_list.conf
 # rm luci-app-vssr/root/etc/dnsmasq.ssr/ad.conf
 # rm luci-app-vssr/root/etc/dnsmasq.ssr/gfw_base.conf
+
+git clone https://github.com/liuwenwv/luci-app-vssr-plus
 # openclash
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash
 # adguardhome
