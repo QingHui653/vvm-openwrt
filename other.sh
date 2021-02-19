29b1b9aaf7584c6a3462d3d0e886f6d584b5fc04
qinghui <qinghui910944453@gmail.com>
2021-02-19 18:54:41 +0800

    bypass

---
+++ other.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+pwd
+ls -l
+echo "开始 DIY 配置"
+pwd
+ls -l
+
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
