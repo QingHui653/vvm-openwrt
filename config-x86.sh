9519c20dc7e046e2bae0671e4bdd3e8bf4bbb596
qinghui <910944453@qq.com>
2021-10-20 14:25:18 +0800

    luci-app-aliyundrive-webdav

--- config-x86.sh
+++ config-x86.sh
@@ -81,6 +81,7 @@ CONFIG_PACKAGE_luci-app-amule=y
 CONFIG_PACKAGE_luci-app-qbittorrent=y
 CONFIG_PACKAGE_luci-app-transmission=n
 CONFIG_PACKAGE_luci-app-baidupcs-web=y
+CONFIG_PACKAGE_luci-app-aliyundrive-webdav=y
 
 # 内网穿透
 CONFIG_PACKAGE_luci-app-frpc=y
