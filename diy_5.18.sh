acdab69b81c2c1387ea45075d1cf006d8893b83e
qinghui <qinghui910944453@gmail.com>
2022-07-12 10:20:58 +0800

    5.18

---
+++ diy_5.18.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+pwd
+ls -l
+echo "开始 DIY 配置"
+pwd
+ls -l
+
+# 修改默认ip
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+#替换使用5.10内核
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.18/g' target/linux/x86/Makefile
