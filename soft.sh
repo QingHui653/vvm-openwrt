5c5b255e1b587188a6e6a88bf3564f3cf717ddb5
QingHui653 <qinghui910944453@gmail.com>
2024-03-02 20:35:03 +0800

    svn已失效，调整为git

--- soft.sh
+++ soft.sh
@@ -3,50 +3,33 @@ echo "开始 github Soft 配置"
 #位于 /home/runner/work/vvm-openwrt/vvm-openwrt/openwrt/
 # openwort 目录下 执行 添加feeds 
 
+# 使用 https://github.com/stupidloud/nanopi-openwrt/blob/master/scripts/merge_packages.sh 重新下载
+
 pwd
 ls -l
 cd package
 pwd
 ls -l
 
-#https://github.com/kenzok8/small-package常用仓库包
-# git clone https://github.com/kenzok8/small-package
-# 无法全部拉取,会导致编译超过6h
-# git clone https://github.com/kenzok8/openwrt-packages
 
 git clone https://github.com/kenzok8/small
-#依赖
-svn co https://github.com/kenzok8/openwrt-packages/trunk/lua-neturl
-svn co https://github.com/kenzok8/openwrt-packages/trunk/gost
-svn co https://github.com/kenzok8/openwrt-packages/trunk/lua-maxminddb
-svn co https://github.com/kenzok8/openwrt-packages/trunk/microsocks
-svn co https://github.com/kenzok8/openwrt-packages/trunk/mosdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/naiveproxy
-svn co https://github.com/kenzok8/openwrt-packages/trunk/redsocks2
-svn co https://github.com/kenzok8/openwrt-packages/trunk/sagernet-core
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/taskd
-svn co https://github.com/kenzok8/openwrt-packages/trunk/tcping
-
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall2
-
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ikoolproxy
-
-# adguardhome
-# svn co https://github.com/rufengsuixing/luci-app-adguardhome/branches/beta/
-
-#atmaterial主题
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-mcat
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-tomato
-
-# openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash/tools/po2lmo
-# git clone -b dev https://github.com/vernesong/OpenClash.git && cp -r OpenClash/luci-app-openclash ./
-
-#mosdns v4 版本
-# rm -rf mosdns && git clone https://github.com/IrineSistiana/mosdns/ -b v4
-#alist 各种网盘
 git clone https://github.com/sbwml/luci-app-alist
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到openwrt/package/openwrt-packages路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" && shift 2
+    rootdir="$PWD"
+    localdir=package/openwrt-packages
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    mv -f "$@" "$rootdir"/"$localdir" && cd "$rootdir"
+}
+
+merge_package master https://github.com/WYC-2020/openwrt-packages luci-app-openclash
