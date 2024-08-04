53a9c7fd6fe7cf3ec71e71c15fcc20529e39f7e5
QingHui653 <qinghui910944453@gmail.com>
2024-08-04 17:22:18 +0800

    6.1内核

--- diy_6.1.sh
+++ diy_6.1.sh
@@ -16,4 +16,5 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 
 #替换使用5.10内核
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
 
