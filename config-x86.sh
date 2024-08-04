dc67c1611d0db6ff4643ca2ebaf0dc1fc9036f80
qinghui <qinghui910944453@gmail.com>
2024-08-04 10:18:40 +0800

    Update config-x86.sh

--- config-x86.sh
+++ config-x86.sh
@@ -91,7 +91,7 @@ CONFIG_PACKAGE_luci-app-qbittorrent=y
 CONFIG_PACKAGE_luci-app-transmission=n
 CONFIG_PACKAGE_luci-app-baidupcs-web=n
 CONFIG_PACKAGE_luci-app-aliyundrive-webdav=n
-CONFIG_PACKAGE_luci-app-alist=y
+CONFIG_PACKAGE_luci-app-alist=n
 CONFIG_PACKAGE_luci-app-store=y
 
 # 内网穿透
