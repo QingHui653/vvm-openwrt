59591ce68eed58e0cbbb1d77fc2f6b7976650b63
qinghui <qinghui910944453@gmail.com>
2024-11-23 01:46:44 +0800

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -19,7 +19,7 @@ git clone https://github.com/kenzok8/small
 # git clone https://github.com/sbwml/luci-app-alist
 
 # theme
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
 
 # eqosplus 网速控制更多功能 
